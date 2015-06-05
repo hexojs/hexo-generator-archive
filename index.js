f6ffd2c387dcb74ccc8c191ce0022c2f4b2a23aa
Tommy Chen <tommy351@gmail.com>
2015-06-05 13:23:06 +0800

    Allow users to set the per_page option to 0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var assign = require('object-assign');
 
 hexo.config.archive_generator = assign({
-  per_page: hexo.config.per_page,
+  per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
   yearly: true,
   monthly: true
 }, hexo.config.archive_generator);
