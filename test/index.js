791538c3c672586ab96778fa59ead66a5836640d
yuhuanzhu <yuhuanzhu@baidu.com>
2015-12-10 15:09:35 +0800

    add tests

--- test/index.js
+++ test/index.js
@@ -12,6 +12,8 @@ describe('Archive generator', function(){
 
   before(function(){
     return Post.insert([
+      {source: 'quux', slug: 'quux', date: new Date(2014, 1, 2)},
+      {source: 'qux', slug: 'qux', date: new Date(2014, 1, 2)},
       {source: 'foo', slug: 'foo', date: new Date(2014, 1, 2)},
       {source: 'bar', slug: 'bar', date: new Date(2013, 5, 6)},
       {source: 'baz', slug: 'baz', date: new Date(2013, 9, 10)},
@@ -26,12 +28,13 @@ describe('Archive generator', function(){
     hexo.config.archive_generator = {
       per_page: 2,
       yearly: true,
-      monthly: true
+      monthly: true,
+      daily: false
     };
 
     var result = generator(locals);
 
-    result.length.should.eql(8);
+    result.length.should.eql(11);
 
     for (var i = 0, len = result.length; i < len; i++){
       result[i].layout.should.eql(['archive', 'index']);
@@ -40,7 +43,7 @@ describe('Archive generator', function(){
 
     result[0].path.should.eql('archives/');
     result[0].data.base.should.eql('archives/');
-    result[0].data.total.should.eql(2);
+    result[0].data.total.should.eql(3);
     result[0].data.current.should.eql(1);
     result[0].data.current_url.should.eql('archives/');
     result[0].data.posts.should.eql(posts.limit(2));
@@ -51,100 +54,115 @@ describe('Archive generator', function(){
 
     result[1].path.should.eql('archives/page/2/');
     result[1].data.base.should.eql('archives/');
-    result[1].data.total.should.eql(2);
+    result[1].data.total.should.eql(3);
     result[1].data.current.should.eql(2);
     result[1].data.current_url.should.eql('archives/page/2/');
-    result[1].data.posts.should.eql(posts.skip(2));
+    result[1].data.posts.should.eql(posts.skip(2).limit(2));
     result[1].data.prev.should.eql(1);
     result[1].data.prev_link.should.eql('archives/');
-    result[1].data.next.should.eql(0);
-    result[1].data.next_link.should.eql('');
+    result[1].data.next.should.eql(3);
+    result[1].data.next_link.should.eql('archives/page/3/');
 
-    result[2].path.should.eql('archives/2013/');
-    result[2].data.base.should.eql('archives/2013/');
-    result[2].data.total.should.eql(2);
-    result[2].data.current.should.eql(1);
-    result[2].data.current_url.should.eql('archives/2013/');
-    result[2].data.posts.should.eql(posts.slice(1, 3));
-    result[2].data.prev.should.eql(0);
-    result[2].data.prev_link.should.eql('');
-    result[2].data.next.should.eql(2);
-    result[2].data.next_link.should.eql('archives/2013/page/2/');
-    result[2].data.year.should.eql(2013);
+    result[2].path.should.eql('archives/page/3/');
+    result[2].data.base.should.eql('archives/');
+    result[2].data.total.should.eql(3);
+    result[2].data.current.should.eql(3);
+    result[2].data.current_url.should.eql('archives/page/3/');
+    result[2].data.posts.should.eql(posts.skip(4));
+    result[2].data.prev.should.eql(2);
+    result[2].data.prev_link.should.eql('archives/page/2/');
+    result[2].data.next.should.eql(0);
+    result[2].data.next_link.should.eql('');
 
-    result[3].path.should.eql('archives/2013/page/2/');
+    result[3].path.should.eql('archives/2013/');
     result[3].data.base.should.eql('archives/2013/');
     result[3].data.total.should.eql(2);
-    result[3].data.current.should.eql(2);
-    result[3].data.current_url.should.eql('archives/2013/page/2/');
-    result[3].data.posts.should.eql(posts.slice(3));
-    result[3].data.prev.should.eql(1);
-    result[3].data.prev_link.should.eql('archives/2013/');
-    result[3].data.next.should.eql(0);
-    result[3].data.next_link.should.eql('');
+    result[3].data.current.should.eql(1);
+    result[3].data.current_url.should.eql('archives/2013/');
+    result[3].data.posts.should.eql(posts.slice(3, 5));
+    result[3].data.prev.should.eql(0);
+    result[3].data.prev_link.should.eql('');
+    result[3].data.next.should.eql(2);
+    result[3].data.next_link.should.eql('archives/2013/page/2/');
     result[3].data.year.should.eql(2013);
 
-    result[4].path.should.eql('archives/2013/06/');
-    result[4].data.base.should.eql('archives/2013/06/');
-    result[4].data.total.should.eql(1);
-    result[4].data.current.should.eql(1);
-    result[4].data.current_url.should.eql('archives/2013/06/');
-    result[4].data.posts.should.eql(posts.slice(2));
-    result[4].data.prev.should.eql(0);
-    result[4].data.prev_link.should.eql('');
+    result[4].path.should.eql('archives/2013/page/2/');
+    result[4].data.base.should.eql('archives/2013/');
+    result[4].data.total.should.eql(2);
+    result[4].data.current.should.eql(2);
+    result[4].data.current_url.should.eql('archives/2013/page/2/');
+    result[4].data.posts.should.eql(posts.slice(5));
+    result[4].data.prev.should.eql(1);
+    result[4].data.prev_link.should.eql('archives/2013/');
     result[4].data.next.should.eql(0);
     result[4].data.next_link.should.eql('');
     result[4].data.year.should.eql(2013);
-    result[4].data.month.should.eql(6);
 
-    result[5].path.should.eql('archives/2013/10/');
-    result[5].data.base.should.eql('archives/2013/10/');
+    result[5].path.should.eql('archives/2013/06/');
+    result[5].data.base.should.eql('archives/2013/06/');
     result[5].data.total.should.eql(1);
     result[5].data.current.should.eql(1);
-    result[5].data.current_url.should.eql('archives/2013/10/');
-    result[5].data.posts.should.eql(posts.slice(1, 2));
+    result[5].data.current_url.should.eql('archives/2013/06/');
+    result[5].data.posts.should.eql(posts.slice(4, 6));
     result[5].data.prev.should.eql(0);
     result[5].data.prev_link.should.eql('');
     result[5].data.next.should.eql(0);
     result[5].data.next_link.should.eql('');
     result[5].data.year.should.eql(2013);
-    result[5].data.month.should.eql(10);
+    result[5].data.month.should.eql(6);
 
-    result[6].path.should.eql('archives/2014/');
-    result[6].data.base.should.eql('archives/2014/');
+    result[6].path.should.eql('archives/2013/10/');
+    result[6].data.base.should.eql('archives/2013/10/');
     result[6].data.total.should.eql(1);
     result[6].data.current.should.eql(1);
-    result[6].data.current_url.should.eql('archives/2014/');
-    result[6].data.posts.should.eql(posts.limit(1));
+    result[6].data.current_url.should.eql('archives/2013/10/');
+    result[6].data.posts.should.eql(posts.slice(3, 4));
     result[6].data.prev.should.eql(0);
     result[6].data.prev_link.should.eql('');
     result[6].data.next.should.eql(0);
     result[6].data.next_link.should.eql('');
-    result[6].data.year.should.eql(2014);
+    result[6].data.year.should.eql(2013);
+    result[6].data.month.should.eql(10);
 
-    result[7].path.should.eql('archives/2014/02/');
-    result[7].data.base.should.eql('archives/2014/02/');
-    result[7].data.total.should.eql(1);
+    result[7].path.should.eql('archives/2014/');
+    result[7].data.base.should.eql('archives/2014/');
+    result[7].data.total.should.eql(2);
     result[7].data.current.should.eql(1);
-    result[7].data.current_url.should.eql('archives/2014/02/');
-    result[7].data.posts.should.eql(posts.limit(1));
+    result[7].data.current_url.should.eql('archives/2014/');
+    result[7].data.posts.should.eql(posts.limit(2));
     result[7].data.prev.should.eql(0);
     result[7].data.prev_link.should.eql('');
-    result[7].data.next.should.eql(0);
-    result[7].data.next_link.should.eql('');
+    result[7].data.next.should.eql(2);
+    result[7].data.next_link.should.eql('archives/2014/page/2/');
     result[7].data.year.should.eql(2014);
-    result[7].data.month.should.eql(2);
+
+    result[8].path.should.eql('archives/2014/page/2/');
+    result[8].data.base.should.eql('archives/2014/');
+    result[8].data.total.should.eql(2);
+    result[8].data.current.should.eql(2);
+    result[8].data.current_url.should.eql('archives/2014/page/2/');
+    result[8].data.posts.should.eql(posts.slice(2, 3));
+    result[8].data.prev.should.eql(1);
+    result[8].data.prev_link.should.eql('archives/2014/');
+    result[8].data.next.should.eql(0);
+    result[8].data.next_link.should.eql('');
+    result[8].data.year.should.eql(2014);
+
+    result[9].data.year.should.eql(2014);
+    result[9].data.month.should.eql(2);
+    result[10].data.year.should.eql(2014);
+    result[10].data.month.should.eql(2);
   });
 
   it('pagination disabled', function(){
     hexo.config.archive_generator = {
       per_page: 0,
       yearly: true,
-      monthly: true
+      monthly: true,
+      daily: false
     };
 
     var result = generator(locals);
-
     result.length.should.eql(6);
 
     for (var i = 0, len = result.length; i < len; i++){
@@ -168,7 +186,7 @@ describe('Archive generator', function(){
     result[1].data.total.should.eql(1);
     result[1].data.current.should.eql(1);
     result[1].data.current_url.should.eql('archives/2013/');
-    result[1].data.posts.should.eql(posts.slice(1));
+    result[1].data.posts.should.eql(posts.slice(3));
     result[1].data.prev.should.eql(0);
     result[1].data.prev_link.should.eql('');
     result[1].data.next.should.eql(0);
@@ -180,7 +198,7 @@ describe('Archive generator', function(){
     result[2].data.total.should.eql(1);
     result[2].data.current.should.eql(1);
     result[2].data.current_url.should.eql('archives/2013/06/');
-    result[2].data.posts.should.eql(posts.slice(2));
+    result[2].data.posts.should.eql(posts.slice(4));
     result[2].data.prev.should.eql(0);
     result[2].data.prev_link.should.eql('');
     result[2].data.next.should.eql(0);
@@ -193,7 +211,7 @@ describe('Archive generator', function(){
     result[3].data.total.should.eql(1);
     result[3].data.current.should.eql(1);
     result[3].data.current_url.should.eql('archives/2013/10/');
-    result[3].data.posts.should.eql(posts.slice(1, 2));
+    result[3].data.posts.should.eql(posts.slice(3, 4));
     result[3].data.prev.should.eql(0);
     result[3].data.prev_link.should.eql('');
     result[3].data.next.should.eql(0);
@@ -206,7 +224,7 @@ describe('Archive generator', function(){
     result[4].data.total.should.eql(1);
     result[4].data.current.should.eql(1);
     result[4].data.current_url.should.eql('archives/2014/');
-    result[4].data.posts.should.eql(posts.limit(1));
+    result[4].data.posts.should.eql(posts.limit(3));
     result[4].data.prev.should.eql(0);
     result[4].data.prev_link.should.eql('');
     result[4].data.next.should.eql(0);
@@ -218,7 +236,7 @@ describe('Archive generator', function(){
     result[5].data.total.should.eql(1);
     result[5].data.current.should.eql(1);
     result[5].data.current_url.should.eql('archives/2014/02/');
-    result[5].data.posts.should.eql(posts.limit(1));
+    result[5].data.posts.should.eql(posts.limit(3));
     result[5].data.prev.should.eql(0);
     result[5].data.prev_link.should.eql('');
     result[5].data.next.should.eql(0);
@@ -231,7 +249,8 @@ describe('Archive generator', function(){
     hexo.config.archive_generator = {
       per_page: 0,
       yearly: false,
-      monthly: true
+      monthly: true,
+      daily: false
     };
 
     var result = generator(locals);
@@ -241,25 +260,59 @@ describe('Archive generator', function(){
     }).should.eql(['archives/']);
   });
 
-  it('monthly disabled', function(){
+  it('monthly enabled and daily enabled', function(){
     hexo.config.archive_generator = {
       per_page: 0,
       yearly: true,
-      monthly: false
+      monthly: true,
+      daily: true
     };
 
     var result = generator(locals);
 
     result.map(function(item){
       return item.path;
-    }).should.eql(['archives/', 'archives/2013/', 'archives/2014/']);
+    }).should.include('archives/', 'archives/2013/', 'archives/2014/', 'archives/2013/06/', 'archives/2013/10/', 'archives/2014/02/', 'archives/2013/06/06/', 'archives/2013/10/10/', 'archives/2014/02/02');
+  });
+
+  describe('monthly disabled', function() {
+    it('daily disabled', function(){
+      hexo.config.archive_generator = {
+        per_page: 0,
+        yearly: true,
+        monthly: false,
+        daily: false
+      };
+
+      var result = generator(locals);
+
+      result.map(function(item){
+        return item.path;
+      }).should.eql(['archives/', 'archives/2013/', 'archives/2014/']);
+    });
+
+    it('daily enabled', function(){
+      hexo.config.archive_generator = {
+        per_page: 0,
+        yearly: true,
+        monthly: false,
+        daily: true
+      };
+
+      var result = generator(locals);
+
+      result.map(function(item){
+        return item.path;
+      }).should.include('archives/', 'archives/2013/', 'archives/2014/', 'archives/2013/06/06/', 'archives/2013/10/10/', 'archives/2014/02/02');
+    });
   });
 
   it('custom pagination_dir', function(){
     hexo.config.archive_generator = {
       per_page: 1,
       yearly: false,
-      monthly: false
+      monthly: false,
+      daily: false
     };
 
     hexo.config.pagination_dir = 'yo';
@@ -268,7 +321,7 @@ describe('Archive generator', function(){
 
     result.map(function(item){
         return item.path;
-    }).should.eql(['archives/', 'archives/yo/2/', 'archives/yo/3/', 'archives/yo/4/']);
+    }).should.eql(['archives/', 'archives/yo/2/', 'archives/yo/3/', 'archives/yo/4/', 'archives/yo/5/', 'archives/yo/6/']);
 
     // Restore config
     hexo.config.pagination_dir = 'page';
